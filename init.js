4d43e4cdf8433402f95d187131894812c51cfc1e
Aldo Cano <aldo.cano29@gmail.com>
2021-06-09 16:01:36 -0500

    Add files via upload

---
+++ init.js
@@ -0,0 +1,70 @@
+const inquirer = require('inquirer');
+
+/**
+ * @exports init
+ */
+module.exports = init;
+
+/**
+ * @function init
+ * @param {Object} appDetails
+ * @returns {Promise}
+ * @description Entry point for creating a new app with the template
+ */
+async function init(appDetails) {
+	// Create template config with defaults
+	const config = {
+		libraryName: appDetails.appName,
+		mainJs: './src/main.js',
+		globals: {
+			apex: 'apex'
+		},
+		srcFolder: './src',
+		distFolder: './build',
+		external: ['apex'],
+		version: '1.0.0'
+	};
+
+	// Ask questions
+	const answers = await inquirer.prompt(getTemplateQuestions(appDetails));
+
+	// Set main answers
+	config.libraryName = answers['library-name'];
+	config.cssExtensions = answers['css-extensions'];
+
+	return config;
+}
+
+/**
+ * @private
+ */
+function getTemplateQuestions(appDetails) {
+	return [
+		{
+			name: 'library-name',
+			type: 'input',
+			message: 'Library name:',
+			default: appDetails.appName,
+			validate: input => {
+				if (/^([A-Za-z\d])+$/.test(input)) {
+					return true;
+				}
+
+				return 'The library name may only include letters and numbers.';
+			}
+		},
+		{
+			name: 'css-extensions',
+			type: 'list',
+			message: 'CSS processors?',
+			default: ['.css'],
+			choices: [
+				{name: 'CSS', value: ['css']},
+				new inquirer.Separator(),
+				{name: 'Sass', value: ['scss']},
+				{name: 'Less', value: ['less']},
+				{name: 'Stylus', value: ['styl']}
+			]
+		}
+	];
+}
---
+++ template/webpack.dev.js
@@ -0,0 +1,166 @@
+const path = require("path");
+const apexnitroConfig = require("./apexnitro.config.json");
+const CopyPlugin = require("copy-webpack-plugin");
+
+let styleRule;
+
+if (apexnitroConfig.cssExtensions.includes('css')) {
+  styleRule = {
+    test: /\.css$/i,
+    use: [
+      {
+        loader: "file-loader",
+        options: {
+          name: `${apexnitroConfig.libraryName}.css`
+        }
+      },
+      { loader: "extract-loader" },
+      { loader: "css-loader?-url" },
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: [
+              [
+                'postcss-preset-env',
+                {},
+              ],
+            ],
+          },
+        },
+      }
+    ]
+  };
+}
+
+if (apexnitroConfig.cssExtensions.includes('scss')) {
+  styleRule = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      {
+        loader: "file-loader",
+        options: {
+          name: `${apexnitroConfig.libraryName}.css`
+        }
+      },
+      { loader: "extract-loader" },
+      { loader: "css-loader?-url" },
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: [
+              [
+                'postcss-preset-env',
+                {},
+              ],
+            ],
+          },
+        },
+      },
+      {
+        loader: "sass-loader"
+      }
+    ]
+  };
+}
+
+if (apexnitroConfig.cssExtensions.includes('less')) {
+  styleRule = {
+    test: /\.less$/,
+    use: [
+      {
+        loader: "file-loader",
+        options: {
+          name: `${apexnitroConfig.libraryName}.css`
+        }
+      },
+      { loader: "extract-loader" },
+      { loader: "css-loader?-url" },
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: [
+              [
+                'postcss-preset-env',
+                {},
+              ],
+            ],
+          },
+        },
+      },
+      {
+        loader: "less-loader"
+      }
+    ]
+  };
+}
+
+if (apexnitroConfig.cssExtensions.includes('styl')) {
+  styleRule = {
+    test: /\.styl$/,
+    use: [
+      {
+        loader: "file-loader",
+        options: {
+          name: `${apexnitroConfig.libraryName}.css`
+        }
+      },
+      { loader: "extract-loader" },
+      { loader: "css-loader?-url" },
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: [
+              [
+                'postcss-preset-env',
+                {},
+              ],
+            ],
+          },
+        },
+      },
+      {
+        loader: "stylus-loader"
+      }
+    ]
+  };
+}
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: [apexnitroConfig.mainCss, apexnitroConfig.mainJs],
+  output: {
+    path: path.resolve(apexnitroConfig.distFolder),
+    library: apexnitroConfig.libraryName,
+    filename: `${apexnitroConfig.libraryName}.js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      styleRule
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(apexnitroConfig.srcFolder, "static"),
+          to: path.resolve(apexnitroConfig.distFolder, "static")
+        }
+      ]
+    })
+  ]
+};
