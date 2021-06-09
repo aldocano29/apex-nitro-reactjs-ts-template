58e9190b6b4f8b5e579c7fec989f68345aabe5b4
Aldo Cano <aldo.cano29@gmail.com>
2021-06-09 18:08:37 -0500

    Update main.tsx

--- template/src/main.tsx
+++ template/src/main.tsx
@@ -1,12 +1,15 @@
 import p1 from "./js/p1";
 import p2 from "./js/p2";
 
+declare const apex: any;
+const $ = apex.jQuery;
+
 export {
   p1,
   p2
 };
 
-apex.jQuery(document).ready(() => {
+$(document).ready(() => {
 	const pageId = Number(document.getElementById("pFlowStepId").value);
   if (pageId === 1) p1.init();
   if (pageId === 2) p2.init();
